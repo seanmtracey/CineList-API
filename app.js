f160b81bfcb7d020396d4e26dd5deaf5013703d3
seanmtracey <seanmtracey@gmail.com>
2016-05-09 01:43:33 +0100

    Added documentation

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
+app.use(express.static('documentation'));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
