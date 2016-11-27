7c0f03f95ec364366a0dab5b867148d0a41bd5aa
seanmtracey <seanmtracey@gmail.com>
2016-11-27 01:24:35 +0000

    Preload 100 most populations in the UK on start.

--- routes/get.js
+++ routes/get.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const debug = require('debug')('api:lib:index');
+const debug = require('debug')('api:routes:get');
 
 const locationInterface = require('../lib/locationInterface');
 const FaF = require('../lib/fafScraper');
--- routes/search.js
+++ routes/search.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const debug = require('debug')('api:lib:index');
+const debug = require('debug')('api:routes:search');
 
 const locationInterface = require('../lib/locationInterface');
 const FaF = require('../lib/fafScraper');
