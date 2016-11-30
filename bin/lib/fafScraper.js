d4081f9168dfbd606e9a8b476751f978f8c4ed98
Sean Tracey <seanmtracey@gmail.com>
2016-11-30 11:32:47 +0000

    Cache results for cinema by postcode

--- bin/lib/fafScraper.js
+++ bin/lib/fafScraper.js
@@ -22,50 +22,68 @@ function getCinemasForPostcode(postcode, attempt){
 	
 	if(postcode === undefined || postcode === "" || postcode === null){
 		debug("getCinemasForPostcode: Request was rejected", postcode);
-		return new Promise.reject("A postcode was not passed to the function");
+		return Promise.reject("A postcode was not passed to the function");
 	}
-	
-	return fetch(fafURL)
-		.then(res => {
-			return res.text();	
-		})
-		.then(html => {
-			const parsedHTML = cheerio.load(html,{
-				normalizeWhitespace: true,
-				xmlMode: true
-			});
-			
-			const cinemaList = Array.from(parsedHTML('ul.cinemas-listings li.cinemaResult'));
 
-			if(cinemaList.length === 0 && attempt < 2){ 
-				debug("There were no cinemas for that postcode. Retrying with truncated postcode");
-				return getCinemasForPostcode(postcode.split(' ')[0], attempt += 1);
-			}
+	const cachedVersion = cache.get(`cinemaLocations:${postcode}`);
+	debug(`Is there a cached list of cinemas for ${postcode}?`, cachedVersion !== undefined);
 
-			const cinemas = cinemaList.map(cinema => {
-				
-				const name = cheerio(cinema).find('span').text();
-				const id = cinema.attribs.rel.replace('#cinemaInfo', '');
-				const distance = parseFloat(cinema.children[1].data.replace(' miles', ''));
-				
-				if(distance < 20){
-					return {
-						name,
-						id,
-						distance
-					};
-				}
-				
-			}).filter(result => {
-				return result === undefined ? false : true;
+	if(cachedVersion){
+
+		const parsedCacheVersion = JSON.parse(cachedVersion);
+		debug(`Returning cached list of cinemas for ${postcode}`, parsedCacheVersion.cinemas);
+		return Promise.resolve(parsedCacheVersion.cinemas);
+
+	} else {
+
+		return fetch(fafURL)
+			.then(res => {
+				return res.text();	
 			})
-			
-			debug(cinemas);
-			
-			return cinemas;
-			
-		})
-	;
+			.then(html => {
+				const parsedHTML = cheerio.load(html,{
+					normalizeWhitespace: true,
+					xmlMode: true
+				});
+				
+				const cinemaList = Array.from(parsedHTML('ul.cinemas-listings li.cinemaResult'));
+
+				if(cinemaList.length === 0 && attempt < 2){ 
+					debug("There were no cinemas for that postcode. Retrying with truncated postcode");
+					return getCinemasForPostcode(postcode.split(' ')[0], attempt += 1);
+				}
+
+				const cinemas = cinemaList.map(cinema => {
+					
+					const name = cheerio(cinema).find('span').text();
+					const id = cinema.attribs.rel.replace('#cinemaInfo', '');
+					const distance = parseFloat(cinema.children[1].data.replace(' miles', ''));
+					
+					if(distance < 20){
+						return {
+							name,
+							id,
+							distance
+						};
+					}
+					
+				}).filter(result => {
+					return result === undefined ? false : true;
+				})
+				
+				debug(cinemas);
+
+				cache.set(`cinemaLocations:${postcode}`, JSON.stringify({
+					state : 'resolved',
+					cinemas
+				}));
+				
+				return cinemas;
+				
+			})
+		;
+
+	}
 	
 }
 
@@ -73,11 +91,11 @@ function getListingForCinemaByID(id, day){
 	
 	if(id === undefined || id === "" || id === null){
 		debug("getListingForCinemaByID: Request was rejected", id);
-		return new Promise.reject("A cinema id was not passed");
+		return Promise.reject("A cinema id was not passed");
 	}
 	
 	day = day || 0;
-	const cachedVersion = cache.get(`${id}:${day}`);
+	const cachedVersion = cache.get(`listings:${id}:${day}`);
 
 	debug(`Is there a cached version of ${id}?`, cachedVersion !== undefined);
 
@@ -125,7 +143,7 @@ function getListingForCinemaByID(id, day){
 
 			if(!data){
 
-				cache.set(`${id}:${day}`, JSON.stringify({
+				cache.set(`listings:${id}:${day}`, JSON.stringify({
 					state : 'invalid',
 					maxAge : (1000 * 60 * 60) * 72
 				}));
