eb0ad7d1ddad20dabbe4bddf6993b1c7220d4774
Sean Tracey <seanmtracey@gmail.com>
2016-11-30 11:43:54 +0000

    Cache postcode validation

--- bin/lib/locationInterface.js
+++ bin/lib/locationInterface.js
@@ -1,41 +1,67 @@
 'use strict';
 const fetch = require('node-fetch');
 const debug = require('debug')('api:lib:locationInterface');
+const LRU = require("lru-cache");
 
 const postCodeAPI = 'http://api.postcodes.io/postcodes';
 const nominatimAPI = 'http://nominatim.openstreetmap.org';
+const cache = LRU({
+	max: 500,
+	length: function (n, key) { return n * 2 + key.length },
+	maxAge: (1000 * 60 * 60) * 24
+});
 
 function validatePostcode (postcode){
 	
 	if(postcode === undefined || postcode === "" || postcode === null){
 		debug("validatePostcode: Request was rejected", postcode);
-		return new Promise.reject("A postcode was not passed to the function");
+		return Promise.reject("A postcode was not passed to the function");
 	}
 	
 	postcode = postcode.replace(" ", "");
 	const postCodeQuery = `${postCodeAPI}/${postcode}/validate`;
 	
-	return fetch(postCodeQuery)
-		.then(res => res.json())
-		.then(data => {
-			
-			debug(data);
-			
-			if(data.result !== undefined){
-				return data.result;
-			} else {
-				throw data;
-			}
-			
-		})
-	;
+	const cachedVersion = cache.get(`validate-postcode:${postcode}`);
+	debug(`Is there a cached information about the postcode ${postcode}?`, cachedVersion !== undefined);
+
+	if(cachedVersion){
+
+		const parsedCacheVersion = JSON.parse(cachedVersion);
+		debug(`Returning cached postcode results for ${postcode}`)
+		return Promise.resolve(parsedCacheVersion.data);
+
+	} else {
+
+		return fetch(postCodeQuery)
+			.then(res => res.json())
+			.then(data => {
+				
+				debug(data);
+				
+				if(data.result !== undefined){
+
+					cache.set(`validate-postcode:${postcode}`, JSON.stringify({
+						state : 'resolved',
+						data : data.result
+					}));
+
+					return data.result;
+				} else {
+					throw data;
+				}
+				
+			})
+		;
+
+	}
+
 }
 
 function getPostCodeForCoordinates (latitude, longitude){
 	
 	if(latitude === undefined || latitude === "" || latitude === null || longitude === undefined || longitude === "" || longitude === null){
 		debug("getPostCodeForCoordinates: Request was rejected", latitude, longitude);
-		return new Promise.reject("Either a latitude or longitude value was omitted. Both are required");
+		return Promise.reject("Either a latitude or longitude value was omitted. Both are required");
 	}
 	
 	const lat = latitude.substring(0, 9);
@@ -63,7 +89,7 @@ function searchForLocation (location){
 	
 	if(location === undefined || location === "" || location === null){
 		debug("searchForLocation: Request was rejected", location);
-		return new Promise.reject("A location was not passed to the function");
+		return Promise.reject("A location was not passed to the function");
 	}
 	
 	const nominatimQuery = `${nominatimAPI}/search?q=${location}&countrycodes=gb&format=json&limit=10`;
