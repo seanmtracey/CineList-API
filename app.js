9ddb5c654655858e7ed477192a50dca5f8b482c9
Sean Tracey <seanmtracey@gmail.com>
2018-10-09 12:53:10 +0100

    Bump version number. Handle 404s a little better.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('cinelist:app');
 const express = require('express');
 const path = require('path');
 const preload = require('./bin/lib/preload');
@@ -25,9 +26,8 @@ preload();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404);
+  res.end();
 });
 
 // error handlers
@@ -36,6 +36,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+    debug('Stack err:', err);
     res.status(err.status || 500);
   });
 }
@@ -44,9 +45,9 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+  res.json({
+    status : err.status,
+    message : `Sorry, an error occurred.`
   });
 });
 
