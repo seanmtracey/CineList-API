ed784162b169227c619bb969a575af240bf2cd68
Sean Tracey <seanmtracey@gmail.com>
2019-08-18 02:58:43 +0100

    Remove unused code.

--- routes/get.js
+++ routes/get.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = express.Router();
 const debug = require('debug')('api:routes:get');
 
-const locationInterface = require('../bin/lib/locationInterface');
 const FaF = require('../bin/lib/fafScraper');
 
 router.get('/times/cinema/:venueID', function(req, res){
